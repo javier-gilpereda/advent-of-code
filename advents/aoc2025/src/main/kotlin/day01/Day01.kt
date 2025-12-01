5b209f27c8e1cdd792540dbbab3cbf758111d092
Javier Gil Pereda <javier-gil.pereda@idealo.de>
2025-12-01 07:56:40 +0100

    Day 1

--- advents/aoc2025/src/main/kotlin/day01/Day01.kt
+++ advents/aoc2025/src/main/kotlin/day01/Day01.kt
@@ -1,5 +1,62 @@
 package com.gilpereda.aoc2025.day01
 
-fun firstTask(input: Sequence<String>): String = TODO()
+fun firstTask(input: Sequence<String>): String =
+    input
+        .map { Operation.from(it) }
+        .fold(Pair(50, 0)) { acc, next ->
+            val result = next.apply(acc.first)
+            val nextCount = if (result == 0) acc.second + 1 else acc.second
+            Pair(result, nextCount)
+        }.second
+        .toString()
 
-fun secondTask(input: Sequence<String>): String = TODO()
+fun secondTask(input: Sequence<String>): String =
+    input
+        .map { Operation.from(it) }
+        .flatMap { it.flatten() }
+        .fold(Pair(50, 0)) { acc, next ->
+            val result = next.apply(acc.first)
+            val nextCount = if (result == 0) acc.second + 1 else acc.second
+            Pair(result, nextCount)
+        }.second
+        .toString()
+
+private const val DIAL_COUNT = 100
+
+sealed interface Operation {
+    fun apply(value: Int): Int
+
+    fun flatten(): List<Operation>
+
+    data class Left(
+        val count: Int,
+    ) : Operation {
+        override fun apply(value: Int): Int {
+            val result = value - (count % DIAL_COUNT)
+            return if (result < 0) {
+                DIAL_COUNT + result
+            } else {
+                result
+            }
+        }
+
+        override fun flatten(): List<Operation> = List(count) { Left(1) }
+    }
+
+    data class Right(
+        val count: Int,
+    ) : Operation {
+        override fun apply(value: Int): Int = (value + count) % DIAL_COUNT
+
+        override fun flatten(): List<Operation> = List(count) { Right(1) }
+    }
+
+    companion object {
+        fun from(line: String): Operation =
+            when (line[0]) {
+                'L' -> Left(line.substring(1).toInt())
+                'R' -> Right(line.substring(1).toInt())
+                else -> throw IllegalArgumentException()
+            }
+    }
+}
--- advents/aoc2025/src/test/kotlin/day01/DayTest.kt
+++ advents/aoc2025/src/test/kotlin/day01/DayTest.kt
@@ -6,13 +6,23 @@ import com.gilpereda.aoc2025.Executable
 class DayTest : BaseTest() {
     override val example: String =
         """
+        L68
+        L30
+        R48
+        L5
+        R60
+        L55
+        L1
+        L99
+        R14
+        L82
         """.trimIndent()
 
-    override val resultExample1: String get() = TODO()
+    override val resultExample1: String = "3"
 
-    override val resultReal1: String = ""
+    override val resultReal1: String = "1147"
 
-    override val resultExample2: String get() = TODO()
+    override val resultExample2: String = "6"
 
     override val resultReal2: String = ""
 
