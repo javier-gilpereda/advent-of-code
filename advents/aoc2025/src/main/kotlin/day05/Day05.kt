0d67146831bb2238b2855f1b261becdfa79195b1
Javier Gil Pereda <javier-gil.pereda@idealo.de>
2025-12-05 13:21:10 +0100

    Day 5

--- advents/aoc2025/src/main/kotlin/day05/Day05.kt
+++ advents/aoc2025/src/main/kotlin/day05/Day05.kt
@@ -1,5 +1,59 @@
 package com.gilpereda.aoc2025.day05
 
-fun firstTask(input: Sequence<String>): String = TODO()
+fun firstTask(input: Sequence<String>): String {
+    val (ranges, ingredientList) =
+        input.fold(Pair(mutableListOf<LongRange>(), mutableListOf<Long>())) { acc, line ->
+            if (line.isBlank()) {
+                acc
+            } else {
+                val ingredients = line.split("-")
+                if (ingredients.size == 2) {
+                    acc.first.add(ingredients[0].toLong()..ingredients[1].toLong())
+                    acc
+                } else {
+                    acc.second.add(ingredients[0].toLong())
+                    acc
+                }
+            }
+        }
 
-fun secondTask(input: Sequence<String>): String = TODO()
+    return ingredientList
+        .count { ingredient ->
+            ranges.any { range -> ingredient in range }
+        }.toString()
+}
+
+fun secondTask(input: Sequence<String>): String =
+    input
+        .takeWhile { it.isNotBlank() }
+        .fold(mutableListOf<LongRange>()) { acc, line ->
+            val newRange = line.split("-").let { it[0].toLong()..it[1].toLong() }
+
+            val intersectBefore = acc.firstOrNull { range -> newRange.first in range }
+            val intersectAfter = acc.firstOrNull { range -> newRange.last in range }
+            acc.firstOrNull { range -> range.first in newRange && range.last in newRange }?.let { acc.remove(it) }
+
+            when {
+                intersectBefore == null && intersectAfter == null -> {
+                    acc.add(newRange)
+                }
+
+                intersectBefore != null && intersectAfter != null -> {
+                    acc.remove(intersectBefore)
+                    acc.remove(intersectAfter)
+                    acc.add(intersectBefore.first..intersectAfter.last)
+                }
+
+                intersectBefore != null -> {
+                    acc.remove(intersectBefore)
+                    acc.add(intersectBefore.first..newRange.last)
+                }
+
+                intersectAfter != null -> {
+                    acc.remove(intersectAfter)
+                    acc.add(newRange.first..intersectAfter.last)
+                }
+            }
+            acc
+        }.sumOf { it.last - it.first + 1 }
+        .toString()
--- advents/aoc2025/src/test/kotlin/day05/DayTest.kt
+++ advents/aoc2025/src/test/kotlin/day05/DayTest.kt
@@ -2,23 +2,90 @@ package com.gilpereda.aoc2025.day05
 
 import com.gilpereda.aoc2025.BaseTest
 import com.gilpereda.aoc2025.Executable
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class DayTest : BaseTest() {
     override val example: String =
         """
+        3-5
+        10-14
+        16-20
+        12-18
+
+        1
+        5
+        8
+        11
+        17
+        32
         """.trimIndent()
 
-    override val resultExample1: String get() = TODO()
+    override val resultExample1: String = "3"
 
-    override val resultReal1: String = ""
+    override val resultReal1: String = "567"
 
-    override val resultExample2: String get() = TODO()
+    override val resultExample2: String = "14"
 
     override val resultReal2: String = ""
 
-    override val input: String = "/day01/input"
+    override val input: String = "/day05/input"
 
     override val run1: Executable = ::firstTask
 
     override val run2: Executable = ::secondTask
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    fun `should work in all cases`(
+        lines: String,
+        expected: String,
+    ) {
+        val result = secondTask(lines.splitToSequence("\n"))
+
+        assertThat(result).isEqualTo(expected)
+    }
+
+    private fun testCases(): Stream<Arguments> =
+        Stream.of(
+            Arguments.of(
+                """
+                1-10
+                7-15
+                12-25
+                """.trimIndent(),
+                "25",
+            ),
+            Arguments.of(
+                """
+                1-10
+                12-25
+                """.trimIndent(),
+                "24",
+            ),
+            Arguments.of(
+                """
+                1-10
+                8-25
+                """.trimIndent(),
+                "25",
+            ),
+            Arguments.of(
+                """
+                1-25
+                8-10
+                """.trimIndent(),
+                "25",
+            ),
+            Arguments.of(
+                """
+                8-10
+                1-25
+                """.trimIndent(),
+                "25",
+            ),
+        )
 }
