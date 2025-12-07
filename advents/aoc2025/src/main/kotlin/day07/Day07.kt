f0564a594c29f467c58d8425632f9f0810d00660
Javier Gil Pereda <javier-gil.pereda@idealo.de>
2025-12-07 11:04:49 +0100

    Day 7

--- advents/aoc2025/src/main/kotlin/day07/Day07.kt
+++ advents/aoc2025/src/main/kotlin/day07/Day07.kt
@@ -1,5 +1,84 @@
 package com.gilpereda.aoc2025.day07
 
-fun firstTask(input: Sequence<String>): String = TODO()
+import com.gilpereda.adventofcode.commons.geometry.Orientation
+import com.gilpereda.adventofcode.commons.geometry.Point
+import com.gilpereda.adventofcode.commons.map.parseToMap
 
-fun secondTask(input: Sequence<String>): String = TODO()
+fun firstTask(input: Sequence<String>): String {
+    lateinit var start: Point
+    val map =
+        input.parseToMap { point, c ->
+            when (c) {
+                'S' -> {
+                    start = point
+                    Cell.START
+                }
+                '^' -> Cell.SPLIT
+                else -> Cell.EMPTY
+            }
+        }
+
+    tailrec fun go(
+        acc: Int,
+        rest: Set<Point>,
+    ): Int =
+        if (rest.isEmpty()) {
+            acc
+        } else {
+            val next = rest.map { it.move(Orientation.SOUTH) }
+            val newAcc = acc + next.count { map.getNullable(it) == Cell.SPLIT }
+            val nextRest =
+                next
+                    .flatMap { point ->
+                        when (map.getNullable(point)) {
+                            null -> emptyList()
+                            Cell.SPLIT ->
+                                listOf(point.move(Orientation.EAST), point.move(Orientation.WEST))
+                                    .filter { point in map }
+                            else -> listOf(point)
+                        }
+                    }.toSet()
+            go(newAcc, nextRest)
+        }
+
+    return go(0, setOf(start)).toString()
+}
+
+fun secondTask(input: Sequence<String>): String {
+    lateinit var start: Point
+    val map =
+        input.parseToMap { point, c ->
+            when (c) {
+                'S' -> {
+                    start = point
+                    Cell.START
+                }
+                '^' -> Cell.SPLIT
+                else -> Cell.EMPTY
+            }
+        }
+
+    val pointToPaths = mutableMapOf<Point, Long>()
+
+    fun pathsFrom(point: Point): Long =
+        when (map.getNullable(point)) {
+            null -> 1L
+            Cell.SPLIT -> pointToPaths.getValue(point)
+            else -> pathsFrom(point.move(Orientation.SOUTH))
+        }
+
+    map.indices.reversed().forEach { point ->
+        val cell = map[point]
+        if (cell == Cell.SPLIT) {
+            pointToPaths[point] = pathsFrom(point.move(Orientation.WEST)) + pathsFrom(point.move(Orientation.EAST))
+        }
+    }
+
+    return pathsFrom(start).toString()
+}
+
+enum class Cell {
+    START,
+    SPLIT,
+    EMPTY,
+}
--- advents/aoc2025/src/test/kotlin/BaseTest.kt
+++ advents/aoc2025/src/test/kotlin/BaseTest.kt
@@ -40,17 +40,17 @@ abstract class BaseTest {
         assertThat(run(input.splitToSequence("\n"))).isEqualTo(expected)
     }
 
-//    @Test
+    @Test
     fun `should work with the example - part 1`() {
         check(example to resultExample1, ::runExample1)
     }
 
-//    @Test
+    @Test
     fun `should return the result - part 1`() {
         assertThat(runReal1(inputSequence)).isEqualTo(resultReal1)
     }
 
-//    @Test
+    @Test
     fun `should work with the example - part 2`() {
         check(example2 to resultExample2, ::runExample2)
     }
